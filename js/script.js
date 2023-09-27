0b11954ac1e4e920d2189eb498aa6a6face217f9
SeiriRyu <56245152+RyuSeiri@users.noreply.github.com>
2023-09-27 22:01:12 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     const gallery = document.querySelector(".image-gallery");
     while (gallery.firstChild) {
-      parent.removeChild(parent.firstChild);
+      parent.removeChild(gallery.firstChild);
     }
     pageImages.forEach((el) => {
       //创建图片项容器
